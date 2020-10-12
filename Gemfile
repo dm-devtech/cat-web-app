2569716be818ecc6de1bb8be33d50d0fb0b5e964
Joshua <joshuamacleod@live.com>
2020-10-12 17:00:33 +0100

    Cat image

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 
 sinatra '2.1.0'
+shotgun '0.9.2'
--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "Hello ben"
+end
+
+get '/cat' do
+  "<div style='border: 2px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
