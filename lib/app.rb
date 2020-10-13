9be27f9babfab21ecc4e6624e6941a8ed779d0f8
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-13 16:26:28 +0100

    Created a new route to interpolate name where parameter given in URL string

--- lib/app.rb
+++ lib/app.rb
@@ -10,7 +10,13 @@ get '/secret' do
   "Hello Me"
 end
 
-get '/cat' do
+get '/random-cat' do
   @random_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @random_name = params[:name]
   erb(:index)
 end
